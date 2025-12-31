d5448f32e039a834ee33394a8a4cd706fc3ee459
Jabir Yimam <jabiryimam1@gmail.com>
2025-12-31 14:12:38 +0300

    polland's algorithm

--- 3_RSA/include/POLLAND.h
+++ 3_RSA/include/POLLAND.h
@@ -4,5 +4,8 @@
 
 #ifndef CRYPTOGRAPHY_POLLAND_H
 #define CRYPTOGRAPHY_POLLAND_H
+#include <utility>
+using ull=unsigned long long;
 
+std::pair<bool, std::pair<ull, ull>> break_rsa_polland(ull N, ull A, int n);
 #endif //CRYPTOGRAPHY_POLLAND_H
--- 3_RSA/src/POLLAND.cpp
+++ 3_RSA/src/POLLAND.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Jabir on 12/31/2025.
 //
+
+#include "../include//POLLAND.h"
+
+#include <numeric>
+
+#include "../../1_An_Introduction_To_Cryptography/include/Tools.h"
+std::pair<bool, std::pair<ull, ull>> break_rsa_polland(ull N, ull a, int n)
+{
+    for (int i=2; i <= n; i++)
+    {
+        a = fast_exponentiation(a, i, N);
+        ull d = std::gcd(a-1, N);
+        if (d==N) return {false, {0,0}};
+        if (d!=1) return {true, {d, N/d}};
+    }
+    return {false, {0,0}};
+};
